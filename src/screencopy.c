7acf2c7aef53d632b37c32229114b7913f8f810e
Andri Yngvason <andri@yngvason.is>
2020-01-14 20:42:48 +0000

    screencopy: Update copyright notice

--- src/screencopy.c
+++ src/screencopy.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Andri Yngvason
+ * Copyright (c) 2019 - 2020 Andri Yngvason
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
