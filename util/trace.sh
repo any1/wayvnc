6c91b38205fd6f43c1985b7a4467eebd62ef1738
Andri Yngvason <andri@yngvason.is>
2020-04-02 20:49:12 +0000

    Add script to run perf trace with USDT

---
+++ util/trace.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+EVENTS="sdt_wayvnc:* sdt_neatvnc:*"
+
+delete_all_events()
+{
+	for e in $EVENTS; do
+		sudo perf probe -d "$e" || true
+	done
+}
+
+add_all_events()
+{
+	for e in $EVENTS; do
+		sudo perf probe "$e"
+	done
+}
+
+sudo perf buildid-cache -a build/wayvnc
+sudo perf buildid-cache -a build/subprojects/neatvnc/libneatvnc.so
+
+delete_all_events
+add_all_events
+
+trap "sudo chown $USER.$USER perf.data*" EXIT
+
+sudo perf record -aR -e ${EVENTS/ /,}
+
