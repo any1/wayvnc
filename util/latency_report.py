06ea9db40ec161b0feee7f47f828e40b9068558d
Andri Yngvason <andri@yngvason.is>
2020-04-03 23:12:03 +0000

    util: latency_report: Add sending fbs to report

--- util/latency_report.py
+++ util/latency_report.py
@@ -56,6 +56,7 @@ class StateTracker:
 
 trackers = [
     StateTracker('Framebuffer update', 'sdt_neatvnc', 'update_fb_start', 'update_fb_done'),
+    StateTracker('Framebuffer update (only sending)', 'sdt_neatvnc', 'send_fb_start', 'send_fb_done'),
     StateTracker('Screencopy', 'sdt_wayvnc', 'screencopy_start', 'screencopy_ready'),
     StateTracker('Render', 'sdt_wayvnc', 'render_framebuffer_start', 'render_framebuffer_end'),
     StateTracker('Copy damage map', 'sdt_wayvnc', 'render_read_damage_start', 'render_read_damage_end'),
